416610983b1832d83d7a2d80981ee6a1c1c1dcd6
JBallin <JBallin@users.noreply.github.com>
2019-05-03 16:40:40 -0700

    Setup cors and dotenv

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
+require('dotenv').config();
 
 const indexRouter = require('./routes/index');
 
@@ -12,11 +14,12 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
+if (!process.env.NODE_ENV) app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors({ origin: process.env.APP_URL || 'http://localhost:3000' }));
 
 app.use('/', indexRouter);
 
