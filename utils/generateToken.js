5a7b8c469ddf0c3f00abcd6b62d081d20e138813
JBallin <JBallin@users.noreply.github.com>
2019-05-05 13:47:51 -0700

    Add generateToken()

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,3 +1,16 @@
-const generateToken = () => {};
+const jwt = require('jsonwebtoken');
+
+const {
+  APPLE_MUSIC_AUTH_KEY: privateKey,
+  APPLE_MUSIC_KEY_ID: keyid,
+  APPLE_TEAM_ID: iss,
+} = process.env;
+const algorithm = 'ES256';
+
+const generateToken = () => {
+  const options = { algorithm, keyid, expiresIn: '3d' };
+  const payload = { iss };
+  return jwt.sign(payload, privateKey, options);
+};
 
 module.exports = { generateToken };
